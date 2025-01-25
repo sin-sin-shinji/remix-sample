c99be62800f8fcb27e3a2eca263a8a8f74bb814c
shin-shin-dev <shin.shin.event@gmail.com>
2025-01-25 10:39:11 +0900

    Add tailwind style test

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,8 @@ import { Link } from '@remix-run/react';
 
 export default function Rootpage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Hello world!</h1>
       <ul>
         <li>
           <Link to="/home">Home</Link>
