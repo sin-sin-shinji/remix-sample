999bc92b64f57aae907ed2732c62405929b545c3
shin-shin-dev <shin.shin.event@gmail.com>
2025-01-25 11:04:51 +0900

    Add button from shadcn-ui

---
+++ app/routes/login.tsx
@@ -0,0 +1,10 @@
+import { Button } from '~/components/ui/button';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>LoginPage</h1>
+      <Button>テスト</Button>
+    </div>
+  );
+}
