708b9102146cf01dc9469060bb9cc48da71549ad
shin-shin-dev <shin.shin.event@gmail.com>
2025-01-25 11:26:19 +0900

    Add Login Form UI

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,10 @@ export default function App() {
     <html lang="jp">
       <head>
         <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
         <Meta />
         <Links />
       </head>
