38977a5abfe3dc27e9b9d5484dc09f88447b2d67
shin-shin-dev <shin.shin.event@gmail.com>
2025-02-08 08:58:49 +0900

    Remove unnecessary file and codes.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,7 @@
-import { Link } from '@remix-run/react';
-
 export default function Rootpage() {
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold mb-4">Hello world!</h1>
-      <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-      </ul>
     </div>
   );
 }
