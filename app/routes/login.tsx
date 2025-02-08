92c29d0e35a6a39fac06c6774e6b555b658fd805
shin-shin-dev <shin.shin.event@gmail.com>
2025-02-08 10:09:47 +0900

    Setup login  and session logic using remix-auth, cookieSessionStorage

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,3 +1,6 @@
+import { Form } from '@remix-run/react';
+import { ActionFunctionArgs, redirect } from '@remix-run/node';
+import { authenticator } from '~/services/auth.server';
 import { Button } from '~/components/ui/button';
 import {
   Card,
@@ -8,42 +11,56 @@ import {
 } from '~/components/ui/card';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
+import { sessionStorage } from '~/services/session.server';
 
 export default function LoginPage() {
   return (
-    <div>
-      <div className="mx-auto w-[350px] mt-40">
+    <div className="mx-auto w-[350px] mt-40">
+      <Form action="/login" method="post">
         <Card className="w-[350px]">
           <CardHeader>
             <CardTitle>ログイン</CardTitle>
           </CardHeader>
           <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="email">メールアドレス</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="メールアドレスを入力してください"
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="password">パスワード</Label>
-                  <Input
-                    id="password"
-                    type="password"
-                    placeholder="パスワードを入力してください"
-                  />
-                </div>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">メールアドレス</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="メールアドレスを入力してください"
+                />
               </div>
-            </form>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">パスワード</Label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="パスワードを入力してください"
+                />
+              </div>
+            </div>
           </CardContent>
           <CardFooter className="flex justify-end">
             <Button>ログイン</Button>
           </CardFooter>
         </Card>
-      </div>
+      </Form>
     </div>
   );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const user = await authenticator.authenticate('user-pass', request);
+
+  const session = await sessionStorage.getSession(
+    request.headers.get('cookie')
+  );
+  session.set('user', user);
+
+  throw redirect('/', {
+    headers: { 'Set-Cookie': await sessionStorage.commitSession(session) },
+  });
 }
---
+++ app/services/auth.server.ts
@@ -0,0 +1,25 @@
+import { Authenticator } from 'remix-auth';
+import { FormStrategy } from 'remix-auth-form';
+import { User } from '~/models/user';
+
+export const authenticator = new Authenticator<User>();
+
+authenticator.use(
+  new FormStrategy(async ({ form }) => {
+    const email = form.get('email');
+    const password = form.get('password');
+
+    if (!email || !password) {
+      throw Error('Both `email` and `password` must be present.');
+    }
+
+    // TODO: ここで対象のUser情報をDBから取得する処理を追加する
+    const user: User = {
+      id: 1,
+      email: 'emailtest',
+      displayName: 'Test User1',
+    };
+    return user;
+  }),
+  'user-pass'
+);
