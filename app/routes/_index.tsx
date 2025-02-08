61e2f14f6b2794a50292eca279c267b865849788
shin-shin-dev <shin.shin.event@gmail.com>
2025-02-08 10:59:53 +0900

    Add style to `logout` link.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,10 @@ export default function Rootpage() {
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold mb-4">Hello world!</h1>
-      <Link to="/logout" className="">
+      <Link
+        to="/logout"
+        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+      >
         ログアウト
       </Link>
     </div>
